1ac16712ce6a1bd2d976ccd748571548165874e7
lucas-gomes1 <mainlucas@gmail.com>
2020-08-01 19:36:04 -0400

    Estado e Cidade

--- src/main/java/com/curso/cursomc/CursomcApplication.java
+++ src/main/java/com/curso/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.curso.cursomc.domain.Categoria;
+import com.curso.cursomc.domain.Cidade;
+import com.curso.cursomc.domain.Estado;
 import com.curso.cursomc.domain.Produto;
 import com.curso.cursomc.repositories.CategoriaRepository;
+import com.curso.cursomc.repositories.CidadeRespository;
+import com.curso.cursomc.repositories.EstadoRespository;
 import com.curso.cursomc.repositories.ProdutoRespository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRespository produtoRespository;
+	@Autowired
+	private EstadoRespository estadoRepository;
+	@Autowired
+	private CidadeRespository cidadeRespository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -40,9 +48,22 @@ public class CursomcApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRespository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRespository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
---
+++ src/main/java/com/curso/cursomc/repositories/EstadoRespository.java
@@ -0,0 +1,12 @@
+package com.curso.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.curso.cursomc.domain.Cidade;
+import com.curso.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRespository extends JpaRepository<Estado, Integer> {
+	
+}
